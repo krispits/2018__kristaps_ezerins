8caeeb7fa9293a2f9813f10b07066c969cedc175
krispits <krispits@gmail.com>
2018-08-16 15:00:53 +0300

    ok

---
+++ scr/hw3For/EvenNumb.java
@@ -0,0 +1,19 @@
+package hw3For;
+
+import java.util.Scanner;
+
+public class EvenNumb {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadiet veselus skaitļus no līdz:");
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+
+        for (int i = num1; i <= num2; i++){
+           if (i%2 == 0){
+            System.out.println(i); }
+        }
+    }
+}
