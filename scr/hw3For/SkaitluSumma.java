0a16ab00de4f6e109250dace897b6cc98c0af703
krispits <krispits@gmail.com>
2018-08-16 14:47:18 +0300

    ok

---
+++ scr/hw3For/SkaitluSumma.java
@@ -0,0 +1,24 @@
+package hw3For;
+
+import java.util.Scanner;
+
+public class SkaitluSumma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ievadiet skaitļus: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = num1; i<=num2; i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+
+
+
+    }
+
+
+}
